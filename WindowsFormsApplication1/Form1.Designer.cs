021b95eb9951d9f3b2b28b3f610c33a6b70b3745
kosiak_man <andrushkin2@live.ru>
2015-11-01 23:33:27 +0300

    Added logic for geting info about file and preparing form to packing

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,39 +29,106 @@
         private void InitializeComponent()
         {
             this.openNewFile = new System.Windows.Forms.Button();
+            this.actionButton = new System.Windows.Forms.Button();
+            this.lableFileName = new System.Windows.Forms.Label();
+            this.lableSizeFile = new System.Windows.Forms.Label();
+            this.lablelFileNameText = new System.Windows.Forms.Label();
+            this.lableFileNameSize = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openNewFile
             // 
             this.openNewFile.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.openNewFile.Location = new System.Drawing.Point(108, 98);
-            this.openNewFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.openNewFile.Location = new System.Drawing.Point(121, 120);
+            this.openNewFile.Margin = new System.Windows.Forms.Padding(4);
             this.openNewFile.Name = "openNewFile";
-            this.openNewFile.Size = new System.Drawing.Size(549, 96);
+            this.openNewFile.Size = new System.Drawing.Size(431, 96);
             this.openNewFile.TabIndex = 0;
             this.openNewFile.Text = "Open a file";
             this.openNewFile.UseVisualStyleBackColor = true;
             this.openNewFile.Click += new System.EventHandler(this.button1_Click);
             // 
+            // actionButton
+            // 
+            this.actionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.actionButton.Enabled = false;
+            this.actionButton.Location = new System.Drawing.Point(175, 95);
+            this.actionButton.Name = "actionButton";
+            this.actionButton.Size = new System.Drawing.Size(317, 94);
+            this.actionButton.TabIndex = 1;
+            this.actionButton.Text = "Start packing";
+            this.actionButton.UseVisualStyleBackColor = true;
+            this.actionButton.Visible = false;
+            this.actionButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lableFileName
+            // 
+            this.lableFileName.AutoSize = true;
+            this.lableFileName.Location = new System.Drawing.Point(126, 243);
+            this.lableFileName.Name = "lableFileName";
+            this.lableFileName.Size = new System.Drawing.Size(76, 37);
+            this.lableFileName.TabIndex = 2;
+            this.lableFileName.Text = "File:";
+            // 
+            // lableSizeFile
+            // 
+            this.lableSizeFile.AutoSize = true;
+            this.lableSizeFile.Location = new System.Drawing.Point(126, 309);
+            this.lableSizeFile.Name = "lableSizeFile";
+            this.lableSizeFile.Size = new System.Drawing.Size(81, 37);
+            this.lableSizeFile.TabIndex = 3;
+            this.lableSizeFile.Text = "Size:";
+            // 
+            // lablelFileNameText
+            // 
+            this.lablelFileNameText.AutoSize = true;
+            this.lablelFileNameText.Location = new System.Drawing.Point(208, 243);
+            this.lablelFileNameText.Name = "lablelFileNameText";
+            this.lablelFileNameText.Size = new System.Drawing.Size(76, 37);
+            this.lablelFileNameText.TabIndex = 4;
+            this.lablelFileNameText.Text = "File:";
+            // 
+            // lableFileNameSize
+            // 
+            this.lableFileNameSize.AutoSize = true;
+            this.lableFileNameSize.Location = new System.Drawing.Point(208, 309);
+            this.lableFileNameSize.Name = "lableFileNameSize";
+            this.lableFileNameSize.Size = new System.Drawing.Size(76, 37);
+            this.lableFileNameSize.TabIndex = 5;
+            this.lableFileNameSize.Text = "File:";
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 36F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(774, 429);
+            this.ClientSize = new System.Drawing.Size(674, 429);
+            this.Controls.Add(this.lableFileNameSize);
+            this.Controls.Add(this.lablelFileNameText);
+            this.Controls.Add(this.lableSizeFile);
+            this.Controls.Add(this.lableFileName);
+            this.Controls.Add(this.actionButton);
             this.Controls.Add(this.openNewFile);
             this.Font = new System.Drawing.Font("Times New Roman", 12.06283F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.MaximumSize = new System.Drawing.Size(800, 500);
-            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(700, 500);
+            this.MinimumSize = new System.Drawing.Size(700, 500);
             this.Name = "mainForm";
             this.Text = "Archiver T1000";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button openNewFile;
+        private System.Windows.Forms.Button actionButton;
+        private System.Windows.Forms.Label lableFileName;
+        private System.Windows.Forms.Label lableSizeFile;
+        private System.Windows.Forms.Label lablelFileNameText;
+        private System.Windows.Forms.Label lableFileNameSize;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,6 +16,30 @@ namespace WindowsFormsApplication1
         public mainForm()
         {
             InitializeComponent();
+            setLabelsVisibility(false);
+        }
+
+        private void setLabelsVisibility(bool isShowLables){
+            lableFileName.Enabled = isShowLables;
+            lableFileName.Visible = isShowLables;
+            
+            lableSizeFile.Enabled = isShowLables;
+            lableSizeFile.Visible = isShowLables;
+
+            lablelFileNameText.Enabled = isShowLables;
+            lablelFileNameText.Visible = isShowLables;
+            
+            lableFileNameSize.Enabled = isShowLables;
+            lableFileNameSize.Visible = isShowLables;
+        }
+        private void setLablesText(string text, int size) {
+            lablelFileNameText.Text = text;
+            lableFileNameSize.Text = size.ToString();
+        }
+        private string getFileName(string fullFileName) { 
+            Array arr = fullFileName.Split('\\');
+            int lastIndex = arr.Length - 1;
+            return arr.GetValue(lastIndex).ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,12 +51,23 @@ namespace WindowsFormsApplication1
                 String fileName = dialog.FileName;
                 try
                 {
+                    openNewFile.Enabled = false;
+                    openNewFile.Visible = false;
+
+                    actionButton.Enabled = true;
+                    actionButton.Visible = true;
                     String text = File.ReadAllText(fileName);
-                    int size = text.Length;
+                    setLabelsVisibility(true);
+                    setLablesText(getFileName(fileName), text.Length);
                 }
                 catch { 
                 }
             }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
